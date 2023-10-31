c42ed9955439ba5e8422d14a4727dee699a25c5a
Michal Odehnal <modehnal@redhat.com>
2023-10-31 16:39:01 +0100

    Getting environment up to date.

--- gnome-terminal/features/environment.py
+++ gnome-terminal/features/environment.py
@@ -42,7 +42,9 @@ def before_all(context) -> None:
     except Exception as error:  # pylint: disable=broad-except
         print(f"Environment error: before_all: {error}")
         traceback.print_exc(file=sys.stdout)
-        sys.exit(1)
+
+        # Save the traceback so that we can use it later when we have html report.
+        context.failed_setup = traceback.format_exc()
 
 
 def before_scenario(context, scenario) -> None:
@@ -59,6 +61,12 @@ def before_scenario(context, scenario) -> None:
     except Exception as error:  # pylint: disable=broad-except
         print(f"Environment error: before_scenario: {error}")
         traceback.print_exc(file=sys.stdout)
+
+        # Attach failed setup from Before Scenario to our html-pretty report.
+        embed_caption = "Failed cleanup in Before Scenario"
+        context.embed("text", traceback.format_exc(), embed_caption)
+
+        # Recommended to correctly place the embed to the Before Scenario.
         sys.exit(1)
 
 
@@ -76,6 +84,10 @@ def after_scenario(context, scenario) -> None:
     except Exception as error:  # pylint: disable=broad-except
         print(f"Environment error: after_scenario: {error}")
         traceback.print_exc(file=sys.stdout)
+
+        # Attach failed setup from After Scenario to our html-pretty report.
+        embed_caption = "Failed cleanup in After Scenario"
+        context.embed("text", traceback.format_exc(), embed_caption)
 
 
 def clean_up_dconf() -> None:
